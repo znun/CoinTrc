f39a7322e6ec1b43095c3a490341deaf0cf402ea
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-10-23 19:49:13 +0600

    commit

---
+++ CoinTrc/CoinTrcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrcApp.swift
+//  CoinTrc
+//
+//  Created by Mahmudul Hasan on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
