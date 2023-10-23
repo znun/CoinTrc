057902435b15f4efc994c61be07d519c655e7c93
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-10-23 20:57:42 +0600

    Setup custom color theme

--- CoinTrc/ContentView.swift
+++ CoinTrc/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40){
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                Text("Secondary Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                Text("Green Color")
+                    .foregroundColor(Color.theme.green)
+            } .font(.headline)
         }
-        .padding()
     }
 }
 
---
+++ CoinTrc/Extentions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CoinTrc
+//
+//  Created by Mahmudul Hasan on 23/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+   static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
