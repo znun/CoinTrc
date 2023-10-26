1dd516b6879c1f563cdf56fa826b81aefefed82d
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-10-26 13:27:47 +0600

    Creating a Coin Model based on JSON response from an API

--- CoinTrc/Core/Home/Views/HomeView.swift
+++ CoinTrc/Core/Home/Views/HomeView.swift
@@ -29,7 +29,7 @@ struct HomeViews_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             HomeView()
-                .navigationBarHidden(true)
+              //  .navigationBarHidden(true)
         }
         
     }
