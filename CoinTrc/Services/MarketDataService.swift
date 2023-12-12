7fa2c2f52ac5414eb0c395808876ca7a3fb463f7
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-12-13 00:49:11 +0600

    Download and display live market data from API

---
+++ CoinTrc/Services/MarketDataService.swift
@@ -0,0 +1,35 @@
+//
+//  MarketDatService.swift
+//  CoinTrc
+//
+//  Created by Mahmudul Hasan on 12/12/23.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        guard let url = URL (string: "https://api.coingecko.com/api/v3/global") else {
+            return
+        }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+         
+            
+        }
+        
+    }
